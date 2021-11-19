6b74aec7763e07d5f872d79fe99e824d7a260437
weskna <koweskna@gmail.com>
2021-11-19 21:20:51 +0100

    Restrict list display width

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container mx-auto p-10">
-    <ul class="bg-yellow-200 shadow-xl max-h-72 overflow-y-auto">
+    <ul
+      class="max-w-lg mx-auto bg-yellow-200 shadow-xl max-h-72 overflow-y-auto"
+    >
       <li
         v-for="listItem in list"
         class="
@@ -15,7 +17,7 @@
         <span>{{ listItem.value }}</span>
         <span>
           <button v-on:click="">
-            <svg id="icon-pencil2" viewBox="0 0 32 32">
+            <svg id="icon-pencil" viewBox="0 0 32 32">
               <path
                 d="M12 20l4-2 14-14-2-2-14 14-2 4zM9.041 27.097c-0.989-2.085-2.052-3.149-4.137-4.137l3.097-8.525 4-2.435 12-12h-6l-12 12-6 20 20-6 12-12v-6l-12 12-2.435 4z"
               ></path>
